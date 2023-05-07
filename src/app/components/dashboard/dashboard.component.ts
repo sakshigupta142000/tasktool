af3eccfadd7ac87df25b0725a0a028ad65342d99
Sakshi Gupta <sakshi.gupta02@telusinternational.com>
2023-05-07 11:27:46 +0530

    fifth commit

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  user={"username":null,"email":null,"profile":null};
+  constructor(private loginService : LoginService){}
+  
+  ngOnInit(): void{
 
+    console.log("---------dashboard----");
+    this.user=this.loginService.getUser();
+    console.log(this.user);
+    console.log("-welcome to the dashboard---");
+  }
 }
